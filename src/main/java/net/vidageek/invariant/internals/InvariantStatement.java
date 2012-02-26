8004d229c1dbb36190db4d46d783418471116413
Jonas Abreu <jonas@vidageek.net>
2012-02-25 23:39:19 -0200

    invariant now visits files according to pattern

--- src/main/java/net/vidageek/invariant/internals/InvariantStatement.java
+++ src/main/java/net/vidageek/invariant/internals/InvariantStatement.java
@@ -2,10 +2,12 @@ package net.vidageek.invariant.internals;
 
 import java.io.File;
 import java.lang.reflect.Method;
+import java.util.regex.Pattern;
 
 import net.vidageek.invariant.FileData;
+import net.vidageek.invariant.Invariant;
 import net.vidageek.mirror.dsl.Mirror;
-import net.vidageek.mirror.exception.ReflectionProviderException;
+import net.vidageek.mirror.exception.MirrorException;
 
 import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
@@ -16,15 +18,29 @@ final public class InvariantStatement extends Statement {
 	private final Class<?> type;
 	private final Method method;
 	private static final Logger log = LoggerFactory.getLogger(InvariantStatement.class);
+	private final Pattern toApply;
 
 	public InvariantStatement(final Method method, final Class<?> type) {
 		this.method = method;
 		this.type = type;
+		toApply = Pattern.compile(method.getAnnotation(Invariant.class).affects());
 	}
 
 	@Override
 	public void evaluate() throws Throwable {
-		evaluateAt(new File("."));
+		evaluate(new File("."));
+	}
+
+	private void evaluate(final File file) throws Throwable {
+		if (file.isFile()) {
+			if (toApply.matcher(file.getAbsolutePath()).matches()) {
+				evaluateAt(file);
+			}
+		} else if (file.isDirectory()) {
+			for (File child : file.listFiles()) {
+				evaluate(child);
+			}
+		}
 	}
 
 	private void evaluateAt(final File file) throws Throwable {
@@ -34,7 +50,7 @@ final public class InvariantStatement extends Statement {
 			Mirror mirror = new Mirror();
 			Object invariantObject = mirror.on(type).invoke().constructor().withoutArgs();
 			mirror.on(invariantObject).invoke().method(method).withArgs(new FileData(file));
-		} catch (ReflectionProviderException e) {
+		} catch (MirrorException e) {
 			Throwable cause = e.getCause();
 			if (AssertionError.class.isAssignableFrom(cause.getClass())) {
 				throw cause;
